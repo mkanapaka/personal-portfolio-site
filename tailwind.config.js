c2855bf5685a130c84d19f13dd7316d182579747
Manohar Kanapaka <manohar.kanapaka@gmail.com>
2024-09-04 18:50:08 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      'blue-900': '#1e3a8a', 
+      'blue-500': '#3b82f6',
+      'sky-300': '#7dd3fc',
+    },
+  },
+  plugins: [],
+}
+
